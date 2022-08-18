df6f9361aa19950c76046c80aaa27781c549d61d
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-08-18 18:58:44 +0530

    debugging firebase calls - part 2

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,10 @@
 import admin from 'firebase-admin'
 
-console.log(`1`, process.env.FIREBASE_PRIVATE_KEY)
-
-process.env.FIREBASE_PRIVATE_KEY &&
-  console.log('2', JSON.parse(process.env.FIREBASE_PRIVATE_KEY))
-
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY
-        ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
-        : undefined,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL
     }),
     databaseURL: process.env.FIREBASE_DATABASE_URL
