81d02144de17d0aa2ef1084a2671abb265089e44
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-11-24 16:15:34 +0530

    fixed ts errors

--- components/icons/saalik.tsx
+++ components/icons/saalik.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-export const Saalik = ({ className, strokeWidth }) => (
+export const Saalik = ({
+  className,
+  strokeWidth
+}: {
+  className: string
+  strokeWidth: string
+}) => (
   <svg
     className={className}
     xmlns="http://www.w3.org/2000/svg"
